3c29e78dab2c77500ccd1a021cd42361c14a1809
BoTom197 <nam.nguyen@edu.turkuamk.fi>
2024-05-27 09:28:50 +0300

    Add files via upload

---
+++ devscode_test.py
@@ -0,0 +1,10 @@
+#File:  dev_test.py
+#Author:    Nguyen Hoai Nam
+#Description:   Code for git testing purpose
+
+print(" Hei, minun nimi on Nguyen Hoai Nam ja täällä testing code")
+print(" user.name: Nguyen Hoai Nam &&& user.email: nam.nguyen@edu.turkuamk.fi")
+print(" Make more information for question 11 Basic with Git Bash")
+print(" Add information from branch Development")
+
+print(" This is branch DEVELOPMENT ")
---
+++ vscode_test.py
@@ -0,0 +1,9 @@
+#File:  git_test.py
+#Author:    Nguyen Hoai Nam
+#Description:   Code for git testing purpose
+
+print(" Hei, minun nimi on Nguyen Hoai Nam ja täällä testing code")
+print(" user.name: Nguyen Hoai Nam &&& user.email: nam.nguyen@edu.turkuamk.fi")
+print(" Make more information for question 11 Basic with Git Bash")
+print(" Add information from branch Development")
+
