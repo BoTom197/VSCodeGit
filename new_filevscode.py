5c254469daba02fb1a978aee69aac85953f94f85
Nguyen Hoai Nam <nam.nguyen@edu.turkuamk.fi>
2024-06-09 14:16:30 +0300

    change something in new_filevscode.py

--- new_filevscode.py
+++ new_filevscode.py
@@ -1,5 +1,7 @@
 print("this is new file python task 3")
 
-print(" Add more code")
+print("Add more code")
 
-print(" We was modified something")
+print("We was modified something")
+
+print("Nothing to lose")
